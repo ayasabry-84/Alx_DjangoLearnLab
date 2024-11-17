40c92d10d805bee822fb2b9ca099d1199a33266d
Aya Sabry <ayasabryy017@gmail.com>
2024-11-17 19:47:46 +0200

    k

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,13 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('relationship_app/', include('relationship_app.urls')),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
-    path('register/', views.register, name='register'),
+    path('auth/', include('relationship_app.urls')),
+
 ]
--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,11 +2,13 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView  # Import the function-based view and class-based view
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # Function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', views.register, name='register'),
 ]
