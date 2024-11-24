121f67451b0b1863ed226804a0ee41eba0a88c4f
Aya Sabry <ayasabryy017@gmail.com>
2024-11-24 22:38:06 +0200

    Second task is done

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,3 +5,7 @@ from .serializers import BookSerializer
 class BookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all() 
+    serializer_class = BookSerializer
