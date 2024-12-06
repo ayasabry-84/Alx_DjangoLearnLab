591cc5b870ec8f99efb80acd5cce2fb506289ae2
Aya Sabry <ayasabryy017@gmail.com>
2024-12-06 14:46:26 +0200

    k

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),  # Custom profile view
 
     path('<int:pk>/', views.post_detail, name='post_detail'),
-    path('<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='add_comment'),
+    path('post/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='add_comment'),
     path('comment/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='edit_comment'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete_comment'),
 
