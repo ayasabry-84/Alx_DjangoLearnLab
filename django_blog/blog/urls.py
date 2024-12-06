8c3afee92389703419e76e93b31d3c0f9f3e1351
Aya Sabry <ayasabryy017@gmail.com>
2024-12-06 08:29:00 +0200

    l

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import register, profile, user_logout
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import BlogPostListView, BlogPostDetailView, BlogPostCreateView, BlogPostUpdateView, BlogPostDeleteView
+from django.contrib.auth import views as auth_views  # Add this import
+
 
 app_name = 'blog'
 
@@ -12,9 +14,10 @@ urlpatterns = [
     path('post/<int:pk>/update/', views.BlogPostUpdateView.as_view(), name='post_update'),  # Update post
     path('post/<int:pk>/delete/', views.BlogPostDeleteView.as_view(), name='post_delete'),  # Delete post
 
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),  # Login URL
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # Logout URL
     path('register/', register, name='register'),
+    
     path('profile/', profile, name='profile'),
     path('logout/', user_logout, name='user_logout'),
 
