bece02c26b6195cebd756c3ef7ac3e8c6f638985
Aya Sabry <ayasabryy017@gmail.com>
2024-12-21 23:10:15 +0200

    edited

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -4,6 +4,15 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer
 
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import IsAuthenticated
+
+
+
 class RegisterView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
@@ -12,17 +21,11 @@ class RegisterView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
-from rest_framework.response import Response
-
 class LoginView(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
         token = Token.objects.get(key=response.data['token'])
         return Response({'token': token.key, 'user_id': token.user_id})
-
-from rest_framework.permissions import IsAuthenticated
 
 class ProfileView(APIView):
     permission_classes = [IsAuthenticated]
