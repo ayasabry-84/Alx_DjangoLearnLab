1036871fbedf003b2afb887e22d674281e35aa96
Aya Sabry <ayasabryy017@gmail.com>
2024-12-21 09:39:16 +0200

    ;

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -9,10 +9,11 @@ class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(**validated_data)
+        user =  User.objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
